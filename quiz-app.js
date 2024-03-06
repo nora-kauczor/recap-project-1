99bc6eca5c538849d7900152fe2fbfe097181fdb
Nora Kauczor <n.kauczor@web.de>
2024-03-06 12:19:22 +0100

    .

--- quiz-app.js
+++ quiz-app.js
@@ -3,3 +3,13 @@ const box1Button = document.querySelector('[data-js="box1-bookmark"]');
 const box2Button = document.querySelector('[data-js="box2-bookmark"]');
 
 const showAnswer = box1Button.addEventListener("click", () => {});
+const showAnswer = box2Button.addEventListener("click", () => {});
+
+// profile page
+const profileMain = document.querySelector('[data-js="profile-page_main"]');
+const darkModeButton = document.querySelector('[data-js="settings-button"]');
+
+// slide statt click??
+darkModeButton.addEventListener("click", () => {
+  profileMain.classList.toggle("dark");
+});
