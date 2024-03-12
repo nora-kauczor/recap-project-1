b1b7abbc1b40f4e90f98836ac697fcf2b1f85730
Nora Kauczor <n.kauczor@web.de>
2024-03-12 11:41:23 +0100

    resolve problems

---
+++ quiz-app.js
@@ -0,0 +1,5 @@
+const box1Button = document.querySelector('[data-js="box1-bookmark"]');
+
+const box2Button = document.querySelector('[data-js="box2-bookmark"]');
+
+const showAnswer = box1Button.addEventListener("click", () => {});
